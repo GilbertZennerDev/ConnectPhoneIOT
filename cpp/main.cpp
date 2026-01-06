323eb8e7c36b6930207a0166fa4ed2afdbb9b10d
gzenner <gzenner@student.42luxembourg.lu>
2026-01-06 11:31:14 +0100

    cpp

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -10,11 +10,56 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
 #include "IOT.hpp"
+
+void IOT::test()
+{
+    std::cout << "Test successful.\n";
+}
+
+void IOT::initPhones(char **av)
+{
+	int i;
+    unsigned int limit = atoi(av[1]);    
+
+	i = -1<
+    while (++i < limit)
+        IOT::Phones.push_back(new Phone());
+}
+*/
+
+#include <vector>
+#include <iostream>
+#include <random>
+using namespace std;
+
+class Phone
+{
+	public:
+	Phone(unsigned int id);
+    unsigned int	id;
+    double          pos[2];
+};
+
+Phone::Phone(unsigned int id)
+{
+	id = id;
+	pos[0] = rand() % 1000;
+	pos[1] = rand() % 1000;
+}
 
 int main(int ac, char **av)
 {
-    IOT iot = IOT(av);
-    iot.test();
+	int					i;
+	unsigned int		limit;
+	vector<Phone *>		Phones;
+
+	i = -1;
+	srand(time(NULL));
+	if (ac != 2) exit(1);
+	limit = atoi(av[1]);
+	while (++i < limit)
+		Phones.push_back(new Phone(i));
     return (0);
 }
