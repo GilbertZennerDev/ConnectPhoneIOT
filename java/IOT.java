fc06f93d7a89e8fdb97fbca18d579e08018b0992
GilbertZennerDev <zennergilbert@gmail.com>
2026-01-24 22:07:45 +0100

    java

--- java/IOT.java
+++ java/IOT.java
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-
+/*
 //the next idea is to allow to trace back how the phones connected
 // and to find the most direct route
 // the idea for the route is the following
@@ -32,15 +32,15 @@ class Phone
 	private:
 	unsigned int	id;
 	t_pos			pos;
-	//vector<Phone *>	neighbours;
+	vector<Phone *>	neighbours;
 	
 	public:
 	Phone(unsigned int _id, unsigned int spread);
 
 	unsigned int 	getId();
 	t_pos 			getPos();
-	//void			addNeighbour(Phone *newneighbour);
-	//void			addNeighbours(vector<Phone *>newneighbours);
+	void			addNeighbour(Phone *newneighbour);
+	void			addNeighbours(vector<Phone *>newneighbours);
 };
 
 
@@ -62,7 +62,7 @@ t_pos	Phone::getPos()
 	return (pos);
 }
 
-/*void Phone::addNeighbour(Phone *newneighbour)
+void Phone::addNeighbour(Phone *newneighbour)
 {
 	neighbours.push_back(newneighbour);
 }
@@ -74,7 +74,7 @@ void Phone::addNeighbours(vector<Phone *>newneighbours)
 	i = -1;
 	while (++i < newneighbours.size())
 		neighbours.push_back(newneighbours[i]);
-}*/
+}
 
 //============================================================
 
@@ -197,6 +197,16 @@ void	trackpath(t_data &data)
 				}
 			}
 		}
+		/*i = -1;
+		while (++i < nearbyphones.size() - 1)
+		{
+			delete nearbyphones[i];
+		}
+		i = -1;
+		while (++i < newphones.size() - 1)
+		{
+			delete newphones[i];
+		}
 }
 
 void initPhones(t_data &data)//vector<Phone *> &Phones, unsigned int limit, unsigned int spread)
@@ -256,4 +266,244 @@ int main(int ac, char **av)
 	trackpath(data);
 	deletePhones(data.phones);
     return (0);
+}
+*/
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+
+import java.util.*;
+import java.util.Map;
+import java.util.List;
+import java.util.Scanner;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+import java.util.concurrent.ConcurrentHashMap;
+
+/*import javafx.scene.Group; 
+import javafx.scene.Scene; 
+import javafx.stage.Stage;
+import javafx.scene.paint.Color;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.chart.XYChart;
+import javafx.scene.chart.NumberAxis; 
+import javafx.scene.layout.StackPane;
+import javafx.application.Application;
+import javafx.scene.chart.ScatterChart; 
+import javafx.scene.canvas.GraphicsContext;
+import static javafx.application.Application.launch;*/
+
+class t_pos
+{
+	public double	x;
+	public double	y;
+	public double	z;
+	public t_pos(){x = 0; y = 0; z = 0;}
+}
+
+class Phone
+{
+	private int	id;
+	public t_pos	pos;
+	
+	Phone(int _id, int spread, Random random)
+	{
+		id = _id;
+		pos = new t_pos();
+		pos.x = random.nextDouble() * spread;
+		pos.y = random.nextDouble() * spread;
+		pos.z = random.nextDouble() * spread;
+	}
+
+	public int 	getId(){return id;}
+	public t_pos getPos(){return pos;}
+};
+
+class Data
+{
+	public int		limit;
+	public int		spread;
+	public int		startid;
+	public int		endid;
+	public int		maxDistance;
+	public List<Phone>		phones;
+	
+	static private void print(final String msg)
+	{
+		System.out.println(msg);
+	}
+	
+	static private void printDouble(final String msg, final double v)
+	{
+		print(msg + " " + Double.toString(v));
+	}
+	
+	static private void printInt(final String msg, final int v)
+	{
+		print(msg + " " + Integer.toString(v));
+	}
+
+	Data(String[] args)
+	{
+		boolean error;
+		phones =  new ArrayList<>();
+		try{
+			limit = Integer.parseInt(args[0]);
+			printInt("debug limit", limit);
+			startid = Integer.parseInt(args[1]);
+			endid = Integer.parseInt(args[2]);
+			maxDistance  = Integer.parseInt(args[3]);
+			spread = Integer.parseInt(args[4]);			
+			error = false;
+			if (limit < 2) error = true;
+			if (startid < 0 || startid > limit - 1) error = true;
+			if (endid < 0 || endid > limit - 1) error = true;
+			if (maxDistance <= 0) error = true;
+			if (spread <= 0) error = true;
+			if (error) {print("Error found."); System.exit(1);}
+		}
+		catch(Exception e)
+		{return ;}
+	}
+}
+
+public class IOT
+{
+	private Random random;
+
+	IOT(){random = new Random();}
+	static private void print(final String msg)
+	{
+		System.out.println(msg);
+	}
+	
+	static private void printDouble(final String msg, final double v)
+	{
+		print(msg + " " + v);
+	}
+	
+	static private void printInt(final String msg, final int v)
+	{
+		print(msg + " " + v);
+	}
+	
+	void initPhones(Data data)
+	{
+		printInt("debug limit2", data.limit);
+		for(int i=0; i < data.limit; i++)
+			data.phones.add(new Phone(i, data.spread, random));
+	}
+	
+	void printPhonePos(Phone phone)
+	{
+		t_pos			currentPos;
+
+		currentPos = phone.getPos();
+		print("Printing Position:\nid : " + phone.getId() + " and Pos: " + currentPos.x + " " + currentPos.y + " " + currentPos.z + "\n");
+	}
+	
+	boolean check_phone_in_neighbours(List<Phone> nearbyphones, int endid)
+	{
+		for (Phone phone: nearbyphones)
+		{
+			if (phone.getId() == endid)
+			{
+					print("Found Phone at " + phone.pos.x + " " + phone.pos.y + "\n");
+					return (true);
+			}
+		}
+		return (false);
+	}
+	
+	/*boolean idInArr(int id, List<Phone> arr)
+	{
+		for (Phone phone: arr)
+			if (phone.getId() == id) return (true);
+		return (false);
+	}*/
+	
+	boolean	goodDistance(Phone currentphone, Phone nearphone, double distanceLimit)
+	{
+			double		x;
+			double		y;
+			double		z;
+			double	distance;
+			t_pos	nearPos;
+			t_pos	currentPos;
+
+			nearPos = nearphone.getPos();
+			currentPos = currentphone.getPos();
+			x = nearPos.x - currentPos.x;
+			y = nearPos.y - currentPos.y;
+			z = nearPos.z - currentPos.z;
+			distance = (x * x + y * y + z *z);
+			return (distance <= distanceLimit * distanceLimit);
+	}
+	
+	boolean	nearANYNearPhone(Phone currentPhone, List<Phone> nearbyphones, int maxDistance)
+	{
+		for (Phone phone: nearbyphones)
+			if (goodDistance(currentPhone, phone, maxDistance)) return (true);
+		return (false);
+	}
+	
+	void	trackpath(Data data)
+	{
+		int	iters;
+		Phone phone;
+		Iterator<Phone>	it;
+		List<Phone>	newphones;
+		List<Phone>	lastnewbyphones;
+
+		iters = 0;
+		lastnewbyphones = new ArrayList<>();
+		printPhonePos(data.phones.get(data.startid));
+		printPhonePos(data.phones.get(data.endid));
+		lastnewbyphones.add(data.phones.get(data.startid));
+		while (!check_phone_in_neighbours(lastnewbyphones, data.endid))
+		{
+			it = data.phones.iterator();
+			newphones = new ArrayList<>();
+			print("iteration: " + ++iters + "\n");
+			while (it.hasNext())
+			{
+				phone = it.next();
+				if (nearANYNearPhone(phone, lastnewbyphones, data.maxDistance))
+				{
+					newphones.add(phone);
+					it.remove();
+				}
+			}
+			if (newphones.size() == 0)
+			{
+				print("No new phones reachable and searched phone cannot be found");
+				break;
+			}
+			else
+			{
+				lastnewbyphones = new ArrayList<>(newphones);
+				print(lastnewbyphones.size() + " new phones found.");
+			}
+		}
+	}
+	
+	private void runIOT(String[] args)
+	{
+		Data data;
+		Random random;
+
+		if (args.length != 5){print("Usage: phonecount startid endid connectiondistance spread"); return ;}
+		data =  new Data(args);
+		printInt("debug limit3", data.limit);
+		initPhones(data);
+		trackpath(data);
+	}
+
+	public static void main(String[] args)
+	{
+		IOT iot = new IOT();
+		iot.runIOT(args);
+	}
 }
---
+++ py/iot.py
@@ -0,0 +1,129 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    iot.py                                             :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: gzenner <gzenner@student.42.fr>            +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2025/08/22 15:02:32 by gzenner           #+#    #+#              #
+#    Updated: 2025/11/25 16:47:32 by gzenner          ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+import random, math, sys
+import matplotlib.pyplot as plt
+
+class Space:
+	def __init__(self, phonescount, startid, endid, distance, spread):
+		self.startid = 	startid
+		self.endid = endid
+		self.phonescount = phonescount
+		self.distance = distance
+		self.limx = spread
+		self.limy = spread
+		self.iters = 0
+		self.checks()
+		self.addphones()
+		self.plotPhones(self.phones)
+		self.trackpath()
+
+	def checks(self):
+		c_badrange = lambda value: value < 0 or value >= self.phonescount 
+		c_distance = lambda distance: distance < 0
+		try:
+			self.startid = int(self.startid)
+			self.endid = int(self.endid)
+			self.phonescount = int(self.phonescount)
+			self.distance = int(self.distance)
+			self.limx = int(self.limx)
+			self.limy = int(self.limy)
+		except Exception as e:
+			print(e); exit()
+		if c_badrange(self.startid) or c_badrange(self.endid) or c_distance(self.distance):
+			print('Bad Input'); exit()
+
+	def addphones(self):
+		self.phones = [Phone(i, random.randint(0, self.limx), random.randint(0, self.limy)) for i in range(self.phonescount)]
+
+	def gooddistance(self, currentphone, nearphone):
+		x = nearphone.pos['x'] - currentphone.pos['x']
+		y = nearphone.pos['y'] - currentphone.pos['y']
+		distance = math.sqrt(x**2+y**2)
+		return distance <= self.distance
+    
+	def near(self, currentphone):
+		for nearphone in self.nearbyphones:
+			if self.gooddistance(currentphone, nearphone): return True
+		return False
+
+	def trackpath(self):
+		samelen = lambda : len(self.nearbyphones) <= len(self.phones)
+		self.nearbyphones = [self.phones[self.startid]]
+		while not self.check_phone_in_neighbours() and samelen():
+			self.iters += 1
+			print(self.iters)
+			self.newnearphones = [phone for phone in self.phones if phone not in self.nearbyphones and self.near(phone)]
+			print('newphones', len(self.newnearphones))
+			if not len(self.newnearphones) and not self.check_phone_in_neighbours():
+				print('No new phones reachable and searched phone cannot be found'); exit()
+			self.nearbyphones += self.newnearphones
+			self.plotPhones()
+			
+
+	def check_phone_in_neighbours(self):			
+		ids = [phone.id for phone in self.nearbyphones]
+		if self.endid in ids: self.find_phone(); exit()
+		if len(self.nearbyphones) >= self.phonescount:
+			print('Phone not found'); exit()
+	    
+	def getphonedata(self, endid):
+		for phone in self.phones:
+			id, posx, posy = phone.sendpos()
+			if id == endid: return id, posx, posy
+
+	def find_phone(self):
+		print('finding phone')
+		id1, pos1x, pos1y = self.getphonedata(self.startid)
+		id2, pos2x, pos2y = self.getphonedata(self.endid)
+		print(id1, 'at pos', pos1x, pos1y, 'found', id2, 'at pos', pos2x, pos2y, 'Iterations:', self.iters)
+	
+	def plotPhones(self):
+		pos = [p.sendpos() for p in self.phones]
+		print("pos", pos)
+		x_start = [self.phones[self.startid].sendpos()[1]]
+		y_start = [self.phones[self.startid].sendpos()[2]]
+		x_end = [self.phones[self.endid].sendpos()[1]]
+		y_end = [self.phones[self.endid].sendpos()[2]]
+		x = [p[1] for p in pos]
+		y = [p[2] for p in pos]
+		x_newnear = [phone.sendpos()[1] for phone in self.newnearphones]
+		y_newnear = [phone.sendpos()[2] for phone in self.newnearphones]		
+		plt.scatter(x, y, color = 'black', marker='*')
+		plt.scatter(x_newnear, y_newnear, color = 'green', marker='+')
+		plt.scatter(x_start, y_start, color = 'blue', marker='$s$')
+		plt.scatter(x_end, y_end, color = 'red',marker='$e$')
+		plt.show()
+
+class Phone:
+    def __init__(self, id, x, y):
+        self.id = id
+        self.pos = {'x': x, 'y': y}
+    def sendpos(self):
+        return self.id, self.pos['x'], self.pos['y']
+
+def run():
+	if len(sys.argv) != 6: print('Usage:', sys.argv[0], 'phonecount', 'startid', 'endid', 'connectiondistance', 'spread'); exit()
+	space = Space(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
+
+if __name__ == "__main__": run()
+
+'''
+distance = 5
+near = lambda currentphone, startphone: abs(startphone.pos['x'] - currentphone.pos['x']) < distance or abs(startphone.pos['y'] - currentphone.pos['y']) < distance
+p1 = Phone(1)
+p2 = Phone(2)
+id1, pos1x, pos1y = p1.sendpos()
+id2, pos2x, pos2y = p2.sendpos()
+print(id1, pos1x, pos1y)
+print(id2, pos2x, pos2y)
+print(near(p1, p2))'''
